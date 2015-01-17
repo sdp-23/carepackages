c33d484f00d52327c7d843220dd90650f1c31c4f
Kunal Sharma <ksharma8@terpmail.umd.edu>
2015-01-17 18:22:32 -0500

    reverting change to carepackages/urls.py

--- carepackages/urls.py
+++ carepackages/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from carepackages import views
 
 urlpatterns = patterns('',
-    url(r'^login$', views.home, name='index'),
+    url(r'^login$', views.login, name='index'),
 )
