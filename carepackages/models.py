11f43bb9d3caebbbf21dba7f343ff59fa1de70e5
Kunal Sharma <ksharma8@terpmail.umd.edu>
2015-01-17 14:23:40 -0500

    fixed problems in models.py

--- carepackages/models.py
+++ carepackages/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+	name = models.CharField(max_length = 150)
+	price = models.DecimalField(max_digits = 10, decimal_places = 2)
+	image_link = models.CharField(max_length = 250)
+
 class CarePackage(models.Model):
 	name = models.CharField(max_length = 150)
 	price = models.DecimalField(max_digits = 10, decimal_places = 2)
@@ -9,12 +14,6 @@ class CarePackage(models.Model):
 	downs = models.IntegerField()
 	image_link = models.CharField(max_length = 250)
 	items = models.ManyToManyField(Item)
-
-
-class Item(models.Model):
-	name = models.CharField(max_length = 150)
-	price = models.DecimalField(max_digits = 10, decimal_places = 2)
-	image_link = models.CharField(max_length = 250)
 
 class User(models.Model):
 	f_id = models.CharField(max_length = 50)
