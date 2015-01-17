e3fb72ec55ecdffd8e8f098396f6b78fb8a166a9
sdp-23 <sdp-23@live.com>
2015-01-17 15:02:31 -0500

    from

--- postmates_api/requests.py
+++ postmates_api/requests.py
@@ -1,4 +1,5 @@
-import requests
+from requests import requests
+
 
 def main():
     payload = {'dropoff_address':'20 McAllister St, San Francisco, CA 94102', 'pickup_address':'101 Market St, San Francisco, CA 94105'}
