d69af4d1f92c13c2b63ab77c0680f4c9da7dff63
Kunal Sharma <ksharma8@terpmail.umd.edu>
2015-01-17 18:01:24 -0500

    ...

--- carepackages/views.py
+++ carepackages/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import simplejson as json
 from django.conf import settings
+from django.template import Template
 
+BASE_DIR = settings.BASE_DIR
 
 # Create your views here.
-def display(request):
-	return HttpResponse(json.dumps(str({"ROOT": settings.STATIC_ROOT, 
-										"URL": settings.STATIC_URL
-	}))[1:-1])
+def login(request):
+	r = Template(open(BASE_DIR + "/assets/index.t").read())
+	return r.render()
+
+
