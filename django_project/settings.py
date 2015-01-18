9321f4c1ee1fda9a83d6bdc385a3e01f36765c23
Kunal Sharma <ksharma8@terpmail.umd.edu>
2015-01-17 19:32:27 -0500

    fixed templates

--- django_project/settings.py
+++ django_project/settings.py
@@ -54,6 +54,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
     'django.core.context_processors.tz',
     'django.core.context_processors.request',
+    'django.core.context_processors.csrf',
     'django.contrib.messages.context_processors.messages',
     'django_facebook.context_processors.facebook',
 )
