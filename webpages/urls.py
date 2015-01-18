9e0e112e37dd63d5359e18196c70ac4b00c53837
Kunal Sharma <ksharma8@terpmail.umd.edu>
2015-01-17 23:49:29 -0500

    temp change for testing

--- webpages/urls.py
+++ webpages/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 import webpages.views as webpages_views
 
 urlpatterns = patterns('',
-	url(r'^$', webpages_views.login, name='login_page'),
-    url(r'^$', webpages_views.home, name='index'),
+	url(r'login$', webpages_views.login, name='login_page'),
+    url(r'$', webpages_views.home, name='index'),
 )
