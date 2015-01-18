41ae9c90c3f44eec259f9f458d41465f11d61157
Kunal Sharma <ksharma8@terpmail.umd.edu>
2015-01-17 23:33:21 -0500

    Update urls.py

--- carepackages/urls.py
+++ carepackages/urls.py
@@ -3,9 +3,6 @@ from django.conf.urls import patterns, url
 import carepackages.views as carepackages_views
 
 urlpatterns = patterns('',
-<<<<<<< HEAD
     url(r'^login$', views.login, name='index'),
-=======
     url(r'^login$', carepackages_views.login, name='index'),
->>>>>>> templates
 )
