5e8417cdbffc81f14fbff7f9bcf8db4db7843066
Kunal Sharma <ksharma8@terpmail.umd.edu>
2015-01-18 03:10:58 -0500

    reordered urls

--- webpages/urls.py
+++ webpages/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 import webpages.views as webpages_views
 
 urlpatterns = patterns('',
-	url(r'login$', webpages_views.login, name='login_page'),
+	url(r'register$', webpages_views.login, name='login_page'),
     url(r'$', webpages_views.home, name='index'),
 )
