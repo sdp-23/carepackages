3edd730c58022d84e934c63ebbbe8aa1cbb2e0fb
Kunal Sharma <ksharma8@terpmail.umd.edu>
2015-01-18 07:01:39 -0500

    cookies

--- carepackages/views.py
+++ carepackages/views.py
@@ -20,21 +20,21 @@ def create_user(request):
     expiry = request.GET.get("expiry").split("/")
     exp_month = expiry[0]
     exp_year = expiry[1]
-    try:
-        stripe.api_key = "sk_test_FdEFjYayNgCmvuaeUc0IAr7X"
-        s=stripe.Customer.create(
-            description="Customer for test@example.com",
-            card={'number': request.GET.get('cc_num'),
-                'exp_month': exp_month,
-                'exp_year': exp_year,
-                'cvc': request.GET.get('cvc'),
-                'name': request.GET.get('user_name'),
-            }
-        )
+    #try:
+    stripe.api_key = "sk_test_FdEFjYayNgCmvuaeUc0IAr7X"
+    s=stripe.Customer.create(
+        description="Customer for test@example.com",
+        card={'number': request.GET.get('cc_num'),
+            'exp_month': exp_month,
+            'exp_year': exp_year,
+            'cvc': request.GET.get('cvc'),
+            'name': request.GET.get('user_name'),
+        }
+    )
         u = User(request.GET.get("fb_id"), s["id"])
         u.save()
-    except stripe.CardError, ce: 
-        return HttpResponse("Failure")
+    #except stripe.CardError, ce: 
+    #    return HttpResponse("Failure")
 
     return HttpResponse("Success")
 
